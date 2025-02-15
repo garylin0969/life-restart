bfbac134861592e273079333933e14acee9e0e60
Gary <garylin0969@gmail.com>
2025-02-16 00:41:42 +0800

    chore: remove Next.js image configuration settings

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,6 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        dangerouslyAllowSVG: true, // 允許 SVG 處理
-        contentDispositionType: 'attachment', // 設定下載方式
-        contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;", // 安全策略
-    },
-};
+const nextConfig = {};
 
 export default withNextIntl(nextConfig);
