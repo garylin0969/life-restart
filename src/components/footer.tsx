91b2f370e355c754d37645c662df29a7b7720543
Gary <garylin0969@gmail.com>
2025-02-15 23:03:34 +0800

    feat: add Taiwan flag emoji to footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import { FaGithub, FaLinkedin } from 'react-icons/fa';
 const Footer = () => {
     return (
         <footer className="p-4 text-center">
-            <p className="text-gray-600 dark:text-gray-400">2025 Developed by Gary Lin</p>
+            <p className="text-gray-600 dark:text-gray-400">2025 Developed by Gary Lin | 🇹🇼</p>
             <div className="flex justify-center gap-4 mt-2">
                 <a href="https://github.com/garylin0969" target="_blank" rel="noopener noreferrer">
                     <FaGithub className="w-6 h-6 text-gray-600 dark:text-gray-400 hover:text-gray-800 dark:hover:text-gray-200" />
