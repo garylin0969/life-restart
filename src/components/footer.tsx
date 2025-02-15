4a27d662d878135e92324f2f2b4dfa9e609b446e
Gary <garylin0969@gmail.com>
2025-02-16 01:02:29 +0800

    chore: update GitHub link in footer to point to project repository

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <footer className="p-4 text-center">
             <p className="text-gray-600 dark:text-gray-400 tracking-wide">2025 Developed by Gary Lin | 🇹🇼</p>
             <div className="flex justify-center gap-4 mt-2">
-                <a href="https://github.com/garylin0969" target="_blank" rel="noopener noreferrer">
+                <a href="https://github.com/garylin0969/life-restart" target="_blank" rel="noopener noreferrer">
                     <FaGithub className="w-6 h-6 text-gray-600 dark:text-gray-400 hover:text-gray-800 dark:hover:text-gray-200" />
                 </a>
                 <a href="https://www.linkedin.com/in/gary-lin-95723a247" target="_blank" rel="noopener noreferrer">
